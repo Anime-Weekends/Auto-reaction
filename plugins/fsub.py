9d1cc81394740d4c95f85a95cb5a036631327437
Jҽϝϝɾҽყ ʂαɱα <jeffreymosesdj@gmail.com>
2025-04-02 15:50:03 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -10,10 +10,10 @@ async def get_fsub(bot, message):
         await bot.get_chat_member(target_channel_id, user_id)
     except UserNotParticipant:
         channel_link = (await bot.get_chat(target_channel_id)).invite_link
-        join_button = InlineKeyboardButton("Join Channel", url=channel_link)
+        join_button = InlineKeyboardButton("Jᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=channel_link)
         keyboard = [[join_button]]
         await message.reply(
-            f"<b>Dᴇᴀʀ Usᴇʀ {message.from_user.mention}!\n\nPʟᴇᴀsᴇ ᴊᴏɪɴ ᴏᴜʀ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍᴇ ! 😊\n\nDᴜᴇ ᴛᴏ sᴇʀᴠᴇʀ ᴏᴠᴇʀʟᴏᴀᴅ, ᴏɴʟʏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ sᴜʙsᴄʀɪʙᴇʀs ᴄᴀɴ ᴜsᴇ ᴛʜɪs ʙᴏᴛ !</b>",
+            f"<b><blockquote>Dᴇᴀʀ Usᴇʀ {message.from_user.mention}!</blockquote>\n<blockquote>Pʟᴇᴀsᴇ ᴊᴏɪɴ ᴏᴜʀ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍᴇ ! 😊</blockquote>\n<blockquote>Dᴜᴇ ᴛᴏ sᴇʀᴠᴇʀ ᴏᴠᴇʀʟᴏᴀᴅ, ᴏɴʟʏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ sᴜʙsᴄʀɪʙᴇʀs ᴄᴀɴ ᴜsᴇ ᴛʜɪs ʙᴏᴛ !</blockquote></b>",
             reply_markup=InlineKeyboardMarkup(keyboard),
         )
         return False
